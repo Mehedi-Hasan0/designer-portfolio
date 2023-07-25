516e0889d7e139ede0e88da953ea3852331ebf63
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-07-25 18:34:43 +0600

    Fix: on scroll to 200px header z index increase

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Container from "../Container";
 import Logo from "./Logo";
 import UserMenu from "./UserMenu";
 
 const Navbar = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const zIndex = scrollPosition >= 180 ? 50 : 10;
+
   return (
-    <header className=" w-full bg-white fixed z-10">
+    <header className={` w-full bg-white fixed z-${zIndex}`}>
       <div className=" py-4">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
